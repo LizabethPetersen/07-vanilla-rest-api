2ba2fa43b55e7039982e1fd103bac962835df220
Lizabeth Petersen <lizpetersen928@gmail.com>
2018-06-19 21:47:46 -0700

    adds modules and starter code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 ' use strict';
 
 require('dotenv').config();
+require('faker');
 
 if (!process.env.NODE_ENV) {
   throw new Error('UNDEFINED NODE_ENV');
--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@
 const server = require('./lib/server');
 const logger = require('./lib/logger');
 
+const PORT = process.env.PORT || 3000;
+
 server.start(process.env.PORT, () => logger.log(logger.INFO, `Listening on port ${PORT}`));
